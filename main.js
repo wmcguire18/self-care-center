ac671eff6a9925f09eb6afe25446eef9c97b7d7c
William McGuire <wmcguire18@gmail.com>
2021-05-29 13:59:30 -0400

    Added variables

---
+++ main.js
@@ -0,0 +1,23 @@
+var messageButton = document.querySelector(".receive-message");
+
+var mantras = [
+  "The truth is out there",
+  "I want to believe",
+  "Trust no one",
+  "Deny everything",
+  "Apology is policy",
+  "Deceive. Inveigle. Obfuscate.",
+  "Resist or serve",
+  "Fight the future",
+  "All lies lead to the truth",
+  "Nothing vanishes without a trace",
+  "They always do what they will",
+  "Not everything dies",
+];
+
+
+
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
